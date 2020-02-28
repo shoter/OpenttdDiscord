1e8886c39efbea0219d8350d5009d9094362b633
shoter <lubiebardzozelki@gmail.com>
2020-02-28 10:17:09 +0100

    update

--- OpenttdDiscord/Program.cs
+++ OpenttdDiscord/Program.cs
@@ -1,29 +1,60 @@
-﻿using OpenttdDiscord.Openttd;
+﻿using Discord;
+using Discord.WebSocket;
+using OpenttdDiscord.Openttd;
 using OpenttdDiscord.Openttd.Udp;
 using System;
+using System.Linq;
 using System.Net;
 using System.Net.Sockets;
+using System.Threading.Tasks;
 
 namespace OpenttdDiscord
 {
     class Program
     {
-        static void Main(string[] args)
+        static DiscordSocketClient client;
+        //static void Main(string[] args)
+        //{
+        //    UdpClient readerClient = new UdpClient();
+        //    var remoteEP = new IPEndPoint(IPAddress.Parse("82.177.95.152"), 3980);
+
+        //    var creator = new UdpPacketCreator();
+        //    var packet = creator.CreatePacket(new PacketUdpClientFindServer());
+
+        //    readerClient.Send(packet.Buffer, packet.Size, remoteEP);
+
+        //    byte[] bytes = readerClient.Receive(ref remoteEP);
+        //    Packet received = new Packet(bytes);    
+
+        //    var reader = new UdpPacketReader();
+        //    var receveived = reader.ReadPacket(received);
+        //    int a = 123;
+
+        //    MainAsync().GetAwaiter().GetResult();
+        //}
+
+        public static async Task Main()
         {
-            UdpClient readerClient = new UdpClient();
-            var remoteEP = new IPEndPoint(IPAddress.Parse("82.177.95.152"), 3980);
+            client = new DiscordSocketClient();
+            client.MessageReceived += MessageReceivedAsync;
 
-            var creator = new UdpPacketCreator();
-            var packet = creator.CreatePacket(new PacketUdpClientFindServer());
+            await client.LoginAsync(TokenType.Bot, "NDY5NjU1NDE5Mjc5OTAwNjky.XlekaA.4wq4iuyVhL8LQDBR7YvpiQge0y0");
+            await client.StartAsync();
 
-            readerClient.Send(packet.Buffer, packet.Size, remoteEP);
-
-            byte[] bytes = readerClient.Receive(ref remoteEP);
-            Packet received = new Packet(bytes);    
-
-            var reader = new UdpPacketReader();
-            var receveived = reader.ReadPacket(received);
-            int a = 123;
+            await Task.Delay(-1);
         }
+
+        // This is not the recommended way to write a bot - consider
+        // reading over the Commands Framework sample.
+        private static async Task MessageReceivedAsync(SocketMessage message)
+        {
+            // The bot should never respond to itself.
+            if (message.Author.Id == client.CurrentUser.Id)
+                return;
+
+            if (message.Content == "!ping")
+                await message.Channel.SendMessageAsync("pong!");
+        }
+
     }
 }

ed4369fbe35b329e526a2a5e6f56952c3e838ff1
shoter <lubiebardzozelki@gmail.com>
2020-03-06 00:46:22 +0100

    Created object pool which I will probably not use

---
+++ OpenttdDiscord.Common/Memory/IObjectPool.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenttdDiscord.Common.Memory
+{
+    public interface IObjectPool<T>
+        where T : class
+    {
+        Task<ObjectPoolItem<T>> GetObject();
+
+        Task PutObject(T item);
+    }
+}
---
+++ OpenttdDiscord.Common/Memory/ObjectPool.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenttdDiscord.Common.Memory
+{
+    public class ObjectPool<T> : IObjectPool<T>
+        where T:class
+    {
+        private readonly IFactory<T> factory;
+        private readonly ConcurrentBag<T> pool = new ConcurrentBag<T>();
+
+        public ObjectPool(IFactory<T> factory)
+        {
+            this.factory = factory;
+        }
+
+        public async Task<ObjectPoolItem<T>> GetObject()
+        {
+            if (this.pool.TryTake(out T item))
+                return new ObjectPoolItem<T>(item, this);
+            item = await factory.Create();
+            this.pool.Add(item);
+
+            return new ObjectPoolItem<T>(item, this);
+        }
+
+        public Task PutObject(T item)
+        {
+            this.pool.Add(item);
+            return Task.CompletedTask;
+        }
+    }
+}

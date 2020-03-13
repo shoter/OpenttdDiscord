f00d3cdd1382cb4f96154e5caa3ea0da3e2d5a04
shoter <lubiebardzozelki@gmail.com>
2020-03-13 14:26:16 +0100

    Fixed sql

--- OpenttdDiscord.Database/discord_chat_channel_servers.sql
+++ OpenttdDiscord.Database/discord_chat_channel_servers.sql
@@ -1,11 +1,10 @@
-CREATE TABLE openttd_test.discord_chat_channel_servers (
-  server_id BIGINT(20) NOT NULL AUTO_INCREMENT,
-  channel_id BIGINT(20) NOT NULL AUTO_INCREMENT,
+CREATE TABLE discord_chat_channel_servers (
+  server_id BIGINT(20) UNSIGNED NOT NULL,
+  channel_id BIGINT(20) UNSIGNED NOT NULL,
   server_name VARCHAR(255) DEFAULT NULL,
   PRIMARY KEY (channel_id, server_id)
-)
-ENGINE = INNODB;
+);
 
-ALTER TABLE openttd_test.discord_chat_channel_servers 
+ALTER TABLE discord_chat_channel_servers 
   ADD CONSTRAINT FK_discord_chat_channel_servers_server_id FOREIGN KEY (server_id)
-    REFERENCES openttd_test.() ON DELETE NO ACTION;
+    REFERENCES servers(id) ON DELETE NO ACTION;

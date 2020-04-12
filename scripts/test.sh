adf01af69ccb0e37e42520e7656e474e5fd43687
shoter <lubiebardzozelki@gmail.com>
2020-04-12 09:45:50 +0200

    test

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,6 @@
-TEST_PROJECTS = ( "OpenttdDiscord.Common.Tests" "OpenttdDiscord.Openttd.Tests" )
+# TEST_PROJECTS = ( "OpenttdDiscord.Common.Tests" "OpenttdDiscord.Openttd.Tests" )
+TEST_PROJECTS[0] = "OpenttdDiscord.Common.Tests"
+TEST_PROJECTS[1] = "OpenttdDiscord.Openttd.Tests"
 
 for i in "${TEST_PROJECTS[@]}"
 do

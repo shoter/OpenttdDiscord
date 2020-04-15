c57b611437d82445222465505fdfa418743f65f2
shoter <lubiebardzozelki@gmail.com>
2020-04-16 00:12:19 +0200

    test

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-docker build -t "openttd_discord:$IMAGE_VERSION" .
-docker tag "openttd_discord:$IMAGE_VERSION" "pir.ja.dom/openttd_discord:$IMAGE_VERSION"
+#docker build -t "openttd_discord:$IMAGE_VERSION" .
+#docker tag "openttd_discord:$IMAGE_VERSION" "pir.ja.dom/openttd_discord:$IMAGE_VERSION"
 docker push "pir.ja.dom/openttd_discord:$IMAGE_VERSION"

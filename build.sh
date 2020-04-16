f8167f6c6d1ff35e679fb6ab81a594fa8922ad0c
shoter <lubiebardzozelki@gmail.com>
2020-04-16 02:02:00 +0200

    test

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
+
+# docker build -t "openttd_discord:$IMAGE_VERSION" .
+# docker tag "openttd_discord:$IMAGE_VERSION" "openttd_discord:$IMAGE_VERSION"
 docker login --username $DOCKER_HUB_USR --password $DOCKER_HUB_PSW
-docker build -t "openttd_discord:$IMAGE_VERSION" .
-docker tag "openttd_discord:$IMAGE_VERSION" "openttd_discord:$IMAGE_VERSION"
 docker push "openttd_discord:$IMAGE_VERSION"

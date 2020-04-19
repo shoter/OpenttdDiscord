a8ef982a3d6d7ff0d65dd7106fa1553211da8f7a
shoter <lubiebardzozelki@gmail.com>
2020-04-19 20:44:53 +0200

    Added tcp to docker name

--- OpenttdDiscord.Testing/Database/ContainerizedMysqlDatabase.cs
+++ OpenttdDiscord.Testing/Database/ContainerizedMysqlDatabase.cs
@@ -27,7 +27,7 @@ namespace OpenttdDiscord.Testing.Database
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
             {
                 client = new DockerClientConfiguration(
-                new Uri("docker:2376"))
+                new Uri("tcp://docker:2376"))
                 .CreateClient();
 
             }

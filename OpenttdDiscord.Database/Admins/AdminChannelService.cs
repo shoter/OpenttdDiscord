ede92d66a4e53704fd94e08c4bdcccd2f774cefe
shoter <lubiebardzozelki@gmail.com>
2020-04-30 01:56:24 +0200

    AdminChannelService

---
+++ OpenttdDiscord.Database/Admins/AdminChannelService.cs
@@ -0,0 +1,39 @@
+﻿using OpenttdDiscord.Database.Servers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenttdDiscord.Database.Admins
+{
+    public class AdminChannelService : IAdminChannelService
+    {
+        public event EventHandler<AdminChannel> Added;
+        public event EventHandler<AdminChannel> Removed;
+
+        private readonly IAdminChannelRepository adminChannelRepository;
+
+        public AdminChannelService(IAdminChannelRepository adminChannelRepository)
+        {
+            this.adminChannelRepository = adminChannelRepository;
+        }
+
+        public async Task<AdminChannel> Add(Server server, ulong channelId)
+        {
+            AdminChannel channel = await this.adminChannelRepository.Insert(server, channelId);
+            this.Added?.Invoke(this, channel);
+            return channel;
+        }
+
+        public Task<List<AdminChannel>> GetAll(ulong guildId) => this.adminChannelRepository.GetAdminChannels(guildId);
+
+        public Task<List<AdminChannel>> GetAll(Server server) => this.adminChannelRepository.GetAdminChannels(server);
+
+        public async Task Remove(AdminChannel adminChannel)
+        {
+            await this.adminChannelRepository.RemoveChannel(adminChannel.Server.Id, adminChannel.ChannelId);
+            this.Removed?.Invoke(this, adminChannel);
+        }
+    }
+}
---
+++ OpenttdDiscord.Database/Admins/IAdminChannelService.cs
@@ -0,0 +1,22 @@
+﻿using OpenttdDiscord.Database.Servers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenttdDiscord.Database.Admins
+{
+    public interface IAdminChannelService
+    {
+        event EventHandler<AdminChannel> Added;
+        event EventHandler<AdminChannel> Removed;
+
+        Task<AdminChannel> Add(Server server, ulong channelId);
+
+        Task<List<AdminChannel>> GetAll(ulong guildId);
+        Task<List<AdminChannel>> GetAll(Server server);
+
+        Task Remove(AdminChannel adminChannel);
+    }
+}

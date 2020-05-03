174f371096e5a00f82031b143236dbebc740940f
shoter <lubiebardzozelki@gmail.com>
2020-05-03 11:47:54 +0200

    Preparing for reports

---
+++ OpenttdDiscord/Reporting/ReportSection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenttdDiscord.Reporting
+{
+    public class ReportSection
+    {
+        public string Name { get; }
+
+        public List<string> Data { get; } = new List<string>();
+
+        public ReportSection(string name)
+        {
+            this.Name = name;
+        }
+
+        public void AddData(string data) => this.Data.Add(data);
+        
+    }
+}

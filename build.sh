6d0430752354722f0d0644f389087c239f1b2afd
shoter0 <lubiebardzozelki@gmail.com>
2020-08-03 11:08:08 +0200

    modified bash scripts to be compat with compose

--- build.sh
+++
@@ -1,2 +0,0 @@
-docker build -t openttd_discord .
-docker build -t openttd-discord-mysql -f Dockerfile .
--- run.sh
+++ run.sh
@@ -1,7 +1,1 @@
-docker stop openttd_discord || true && docker rm openttd_discord || true
-docker run -d --name="openttd_discord" \
- -e ottd_discord_token="$ottd_discord_token" \
- -e ottd_discord_connectionstring="$ottd_discord_connectionstring" \
- --restart always \
- openttd_discord
-docker logs -f openttd_discord
+docker-compose up -d -e ottd_discord_token="$ottd_discord_token"

b176a1eb7b191c56895fd363ed8a710a75f205d8
Damian Laczak <shoter@users.noreply.github.com>
2023-02-12 07:42:15 +0100

    test: faulty testContainers

--- OpenttdDiscord.Database.Tests/Servers/OttdServerRepositoryShould.cs
+++ OpenttdDiscord.Database.Tests/Servers/OttdServerRepositoryShould.cs
@@ -1,4 +1,5 @@
 ﻿using DotNet.Testcontainers.Builders;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,12 +15,34 @@ namespace OpenttdDiscord.Database.Tests.Servers
         public async Task test()
         {
             var container = new ContainerBuilder()
+                .WithName("moja_dupa")
                 .WithImage("ottd_discord_test_database")
                 .WithAutoRemove(true)
-                .WithPortBinding(5432, assignRandomHostPort: true)
+                .WithCleanUp(true)
+                .WithPortBinding(5432, true)
                 .Build();
 
-            var _ = container.StartAsync();
+            await container.StartAsync().ConfigureAwait(false);
+
+            var port = container.GetMappedPublicPort(5432);
+
+            var connectionString = "User ID=openttd;" +
+                "Password=secret-pw;" +
+                $"Host=localhost;Port={port};" +
+                "Database=test;Pooling=true;Min Pool Size=0;" +
+                "Max Pool Size=100;Connection Lifetime=0;";
+
+            var optionsBuilder = new DbContextOptionsBuilder<OttdContext>();
+            optionsBuilder.UseNpgsql(Environment.GetEnvironmentVariable(connectionString), x =>
+            {
+                x.MigrationsHistoryTable("__MigrationHistory");
+            });
+
+            var context = new OttdContext(optionsBuilder.Options);
+
+            await context.Database.EnsureCreatedAsync();
+
+            int a = 5;
         }
     }
 }

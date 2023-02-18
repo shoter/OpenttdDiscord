2a8af655915c290e22ca90f2b21999df19dee2bf
Damian Laczak <shoter@users.noreply.github.com>
2023-02-18 16:21:18 +0100

    feat: ValidationErrorEmbedBuilder

--- OpenttdDiscord.Validation/ValidationError.cs
+++ OpenttdDiscord.Validation/ValidationError.cs
@@ -7,9 +7,12 @@ namespace OpenttdDiscord.Validation
     {
         public string Reason { get; set; }
 
+        public List<ValidationFailure> Errors { get; }
+
         public ValidationError(ValidationResult validationResult)
         {
             Reason = validationResult.Errors.First().ErrorMessage;
+            Errors = validationResult.Errors;
         }
     }
 }
---
+++ OpenttdDiscord.Validation/ValidationErrorEmbedBuilder.cs
@@ -0,0 +1,23 @@
+﻿using Discord;
+
+namespace OpenttdDiscord.Validation
+{
+    public class ValidationErrorEmbedBuilder
+    {
+        public Embed BuildEmbed(ValidationError validationError)
+        {
+            EmbedBuilder embedBuilder = new();
+
+            embedBuilder
+                .WithTitle("Validation failed")
+                .WithDescription("Data you have provided was wrong and could not be parsed");
+            
+            foreach(var e in validationError.Errors)
+            {
+                embedBuilder.AddField(e.PropertyName, e.ErrorMessage);
+            }
+
+            return embedBuilder.Build();
+        }
+    }
+}

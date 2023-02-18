be86babfd51613f2ae1eac432544a08e977b3133
Damian Laczak <shoter@users.noreply.github.com>
2023-02-18 16:26:03 +0100

    feat: RegisterOttdServerUseCase validation

--- OpenttdDiscord.Infrastructure/Servers/RegisterOttdServerUseCase.cs
+++ OpenttdDiscord.Infrastructure/Servers/RegisterOttdServerUseCase.cs
@@ -5,6 +5,8 @@ using OpenttdDiscord.Base.Ext;
 using OpenttdDiscord.Database.Servers;
 using OpenttdDiscord.Domain.Security;
 using OpenttdDiscord.Domain.Servers;
+using OpenttdDiscord.Validation;
+using OpenttdDiscord.Validation.Ottd;
 
 namespace OpenttdDiscord.Infrastructure.Servers
 {
@@ -12,6 +14,7 @@ namespace OpenttdDiscord.Infrastructure.Servers
     {
         private readonly IOttdServerRepository ottdServerRepository;
         private readonly ILogger logger;
+        private readonly OttdValidator<OttdServerValidator, OttdServer> validator = new(new());
 
         public RegisterOttdServerUseCase(
             ILogger<RegisterOttdServerUseCase> logger,
@@ -24,19 +27,27 @@ namespace OpenttdDiscord.Infrastructure.Servers
 
         public async Task<EitherUnit> Execute(UserRights userRights, OttdServer server)
         {
-            logger.LogTrace("Executing with {0} for\n{1}", userRights, server);
-            if(userRights.UserLevel != UserLevel.Admin)
-            {
-                return HumanReadableError.EitherUnit("Cannot execute this command as non-admin user!");
-            }
+            return await validator.Validate(server)
+                .Bind(_ =>
+                {
+                    logger.LogTrace("Executing with {0} for\n{1}", userRights, server);
+                    if (userRights.UserLevel != UserLevel.Admin)
+                    {
+                        return HumanReadableError.EitherUnit("Cannot execute this command as non-admin user!");
+                    }
+                    return Unit.Default;
+                })
+                .BindAsync<IError, Unit, Unit>(async _ =>
+                {
+                    var existing = await ottdServerRepository.GetServerByName(server.Name);
+                    if (existing.IsRight)
+                    {
+                        return new HumanReadableError("Server with this name already exists!");
+                    }
 
-            var existing = await ottdServerRepository.GetServerByName(server.Name);
-            if(existing.IsRight)
-            {
-                return new HumanReadableError("Server with this name already exists!");
-            }
-
-            return await ottdServerRepository.InsertServer(server);
+                    return Unit.Default;
+                })
+                .BindAsync(_ => ottdServerRepository.InsertServer(server));
         }
     }
 }

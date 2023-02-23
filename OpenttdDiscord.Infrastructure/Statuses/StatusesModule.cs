c1f0ee6048f1f9881c1684933c113b75d14de211
Damian Laczak <shoter@users.noreply.github.com>
2023-02-24 00:33:51 +0100

    fix: dependencies

--- OpenttdDiscord.Infrastructure/Statuses/StatusesModule.cs
+++ OpenttdDiscord.Infrastructure/Statuses/StatusesModule.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using OpenttdDiscord.Database.Statuses;
+using OpenttdDiscord.Infrastructure.Discord;
 using OpenttdDiscord.Infrastructure.Modularity;
+using OpenttdDiscord.Infrastructure.Statuses.Commands;
+using OpenttdDiscord.Infrastructure.Statuses.Runners;
 using OpenttdDiscord.Infrastructure.Statuses.UseCases;
 
 namespace OpenttdDiscord.Infrastructure.Statuses
@@ -9,6 +13,7 @@ namespace OpenttdDiscord.Infrastructure.Statuses
         public void RegisterDependencies(IServiceCollection services)
         {
             services
+                .AddScoped<IStatusMonitorRepository, StatusMonitorRepository>()
                 .RegisterUseCases()
                 .RegisterRunners()
                 .RegisterCommands();
@@ -17,22 +22,22 @@ namespace OpenttdDiscord.Infrastructure.Statuses
 
     internal static class StatusesModuleExtensions
     {
+
         public static IServiceCollection RegisterUseCases(this IServiceCollection services)
         {
             services.AddScoped<IRegisterStatusMonitorUseCase, RegisterStatusMonitorUseCase>();
-
             return services;
         }
 
         public static IServiceCollection RegisterRunners(this IServiceCollection services)
         {
-
+            services.AddScoped<RegisterStatusMonitorRunner>();
             return services;
         }
 
         public static IServiceCollection RegisterCommands(this IServiceCollection services)
         {
-
+            services.AddSingleton<IOttdSlashCommand, RegisterStatusMonitorCommand>();
             return services;
         }
     }

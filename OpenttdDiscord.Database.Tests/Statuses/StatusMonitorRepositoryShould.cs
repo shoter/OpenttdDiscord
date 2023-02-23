16413988539ca4e2fe40796fee29bf9083f0c623
Damian Laczak <shoter@users.noreply.github.com>
2023-02-23 03:34:24 +0100

    test: all tests for status monitor

--- OpenttdDiscord.Database.Tests/Statuses/StatusMonitorRepositoryShould.cs
+++ OpenttdDiscord.Database.Tests/Statuses/StatusMonitorRepositoryShould.cs
@@ -38,7 +38,64 @@ namespace OpenttdDiscord.Database.Tests.Statuses
                 .Right();
 
             Assert.Single(retrievedMonitors);
-            Assert.Equal(expectedMonitor, retrievedMonitors.First());
+            Equal(expectedMonitor, retrievedMonitors.First());
+        }
+
+        [Fact]
+        public async Task RemoveMonitorFromDatabase()
+        {
+            var repository = await CreateRpeository();
+            var server = await CreateServer();
+            var expectedMonitor = fix.Create<StatusMonitor>();
+            expectedMonitor = expectedMonitor with
+            {
+                ServerId = server.Id,
+                LastUpdateTime = expectedMonitor.LastUpdateTime.ToUniversalTime()
+            };
+
+            (await
+            (from _1 in repository.Insert(expectedMonitor)
+             from _2 in repository.RemoveStatusMonitor(server.Id, expectedMonitor.ChannelId)
+             select _2)
+             ).ThrowIfError();
+
+            List<StatusMonitor> retrievedMonitors = (await repository.GetStatusMonitors(expectedMonitor.ServerId))
+                .ThrowIfError()
+                .Right();
+
+            Assert.Empty(retrievedMonitors);
+        }
+
+        [Fact]
+        public async Task UpdateMonitorInDatabase()
+        {
+            var repository = await CreateRpeository();
+            var server = await CreateServer();
+            var initialMonitor = fix.Create<StatusMonitor>();
+            initialMonitor = initialMonitor with
+            {
+                ServerId = server.Id,
+                LastUpdateTime = initialMonitor.LastUpdateTime.ToUniversalTime()
+            };
+
+            var updatedMonitor = initialMonitor with
+            {
+                MessageId = 2137,
+                LastUpdateTime = DateTime.UtcNow
+            };
+
+            (await
+            (from _1 in repository.Insert(initialMonitor)
+             from _2 in repository.UpdateStatusMonitor(updatedMonitor)
+             select _2)
+             ).ThrowIfError();
+
+            List<StatusMonitor> retrievedMonitors = (await repository.GetStatusMonitors(initialMonitor.ServerId))
+                .ThrowIfError()
+                .Right();
+
+            Assert.Single(retrievedMonitors);
+            Equal(updatedMonitor, retrievedMonitors.First());
         }
 
         private async Task<StatusMonitorRepository> CreateRpeository([CallerMemberName] string? databaseName = null)
@@ -54,7 +111,15 @@ namespace OpenttdDiscord.Database.Tests.Statuses
             var server = fix.Create<OttdServer>();
             (await repository.InsertServer(server)).ThrowIfError();
             return server;
-
         }
+
+        private void Equal(StatusMonitor lhm, StatusMonitor rhm)
+        {
+            Assert.Equal(lhm.ServerId, rhm.ServerId);
+            Assert.Equal(lhm.ChannelId, rhm.ChannelId);
+            Assert.Equal(lhm.MessageId, rhm.MessageId);
+            Assert.Equal(lhm.LastUpdateTime, rhm.LastUpdateTime, TimeSpan.FromMilliseconds(1));
+        }
+
     }
 }

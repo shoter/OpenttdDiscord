603fbbeac99a4d890d0ca454d25522ad290755c9
Damian Laczak <shoter@users.noreply.github.com>
2023-03-02 01:16:59 +0100

    docker: proper tags? maybe?

--- run.pi.sh
+++ run.pi.sh
@@ -1,2 +1,2 @@
-docker-compose build --progress plain --build-arg BUILD_IMG=mcr.microsoft.com/dotnet/sdk:6.0.406-bullseye-slim-arm32v7 --build-arg RUN_IMG=mcr.microsoft.com/dotnet/aspnet:6.0.14-bullseye-slim-arm32v7
+docker-compose build --progress plain --build-arg BUILD_IMG=mcr.microsoft.com/dotnet/sdk:6.0.406-bullseye-slim-arm64v8 --build-arg RUN_IMG=mcr.microsoft.com/dotnet/aspnet:6.0.14-bullseye-slim-arm64v8
 docker-compose up --no-build -d 

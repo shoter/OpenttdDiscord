3df831a51aa15e90f750329ea70ad1f55a5bb0b7
Damian Laczak <shoter@users.noreply.github.com>
2023-03-05 00:39:52 +0100

    feat: barebones RconChannelActor

---
+++ OpenttdDiscord.Infrastructure/Rcon/Actors/RconChannelActor.cs
@@ -0,0 +1,44 @@
+﻿using Akka.Actor;
+using Discord;
+using Discord.WebSocket;
+using OpenTTDAdminPort;
+using OpenttdDiscord.Domain.Rcon;
+using OpenttdDiscord.Domain.Servers;
+
+namespace OpenttdDiscord.Infrastructure.Rcon.Actors
+{
+    internal class RconChannelActor : ReceiveActorBase
+    {
+        private readonly RconChannel channel;
+
+        private readonly OttdServer server;
+
+        private readonly IAdminPortClient client;
+
+        private readonly IMessageChannel messageChannel;
+
+        public RconChannelActor(
+            IServiceProvider sp,
+            RconChannel channel,
+            OttdServer server,
+            IAdminPortClient client,
+            DiscordSocketClient discord)
+            : base(sp)
+        {
+            this.channel = channel;
+            this.server = server;
+            this.client = client;
+            this.messageChannel = (IMessageChannel)discord.GetChannel(channel.ChannelId);
+
+            Ready();
+        }
+
+        public static Props Create(IServiceProvider sp, RconChannel channel, OttdServer server, IAdminPortClient client, DiscordSocketClient discord)
+            => Props.Create(() => new RconChannelActor(sp, channel, server, client, discord));
+
+        private void Ready()
+        {
+
+        }
+    }
+}

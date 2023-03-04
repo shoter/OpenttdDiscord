9f77546ef52d883bea77bbf6988c41979ad0e144
Damian Laczak <shoter@users.noreply.github.com>
2023-03-04 14:11:53 +0100

    feat: RegisterRconChannelRunner

--- OpenttdDiscord.Infrastructure/Rcon/Runners/RegisterRconChannelRunner.cs
+++ OpenttdDiscord.Infrastructure/Rcon/Runners/RegisterRconChannelRunner.cs
@@ -7,7 +7,9 @@ using Discord.WebSocket;
 using LanguageExt;
 using OpenttdDiscord.Base.Basics;
 using OpenttdDiscord.Base.Ext;
+using OpenttdDiscord.Domain.Rcon.UseCases;
 using OpenttdDiscord.Domain.Security;
+using OpenttdDiscord.Domain.Servers.UseCases;
 using OpenttdDiscord.Infrastructure.Discord.Responses;
 using OpenttdDiscord.Infrastructure.Discord.Runners;
 
@@ -15,9 +17,30 @@ namespace OpenttdDiscord.Infrastructure.Rcon.Runners
 {
     internal class RegisterRconChannelRunner : OttdSlashCommandRunnerBase
     {
+        private readonly IRegisterRconChannelUseCase registerRconChannelUseCase;
+
+        private readonly IGetServerByNameUseCase getServerByNameUseCase;
+
+        public RegisterRconChannelRunner(
+            IRegisterRconChannelUseCase registerRconChannelUseCase,
+            IGetServerByNameUseCase getServerByNameUseCase)
+        {
+            this.registerRconChannelUseCase = registerRconChannelUseCase;
+            this.getServerByNameUseCase = getServerByNameUseCase;
+        }
+
         protected override EitherAsync<IError, ISlashCommandResponse> RunInternal(SocketSlashCommand command, User user, ExtDictionary<string, object> options)
         {
-            throw new NotImplementedException();
+            ulong guildId = command.GuildId!.Value;
+            ulong channelId = command.ChannelId!.Value;
+
+            string serverName = options.GetValueAs<string>("server-name");
+            string prefix = options.GetValueAs<string>("prefix");
+
+            return
+                from server in getServerByNameUseCase.Execute(user, serverName, guildId)
+                from _1 in registerRconChannelUseCase.Execute(user, server.Id, guildId, channelId, prefix)
+                select (ISlashCommandResponse)new TextCommandResponse("Rcon channel registered!");
         }
     }
 }

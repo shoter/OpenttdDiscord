247fdc99580890b10552e70a331ece67293ba369
Damian Laczak <shoter@users.noreply.github.com>
2023-03-04 23:38:16 +0100

    feat: IGetRconChannelUseCase

---
+++ OpenttdDiscord.Domain/Rcon/UseCases/IGetRconChannelUseCase.cs
@@ -0,0 +1,12 @@
+﻿using LanguageExt;
+using OpenttdDiscord.Base.Ext;
+
+namespace OpenttdDiscord.Domain.Rcon.UseCases
+{
+    public interface IGetRconChannelUseCase
+    {
+        EitherAsync<IError, RconChannel> Execute(Guid serverId, ulong guildId, ulong channelId);
+
+        EitherAsync<IError, List<RconChannel>> Execute(Guid serverId, ulong guildId);
+    }
+}

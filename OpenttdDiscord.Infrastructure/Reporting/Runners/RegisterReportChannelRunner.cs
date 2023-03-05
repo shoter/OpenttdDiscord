eae36c8cfd1b65b6ab93c07faa9c0691bdc5a00e
Damian Laczak <shoter@users.noreply.github.com>
2023-03-06 00:48:23 +0100

    feat: RegisterReportChannelCommand

---
+++ OpenttdDiscord.Infrastructure/Reporting/Runners/RegisterReportChannelRunner.cs
@@ -0,0 +1,18 @@
+﻿using Discord.WebSocket;
+using LanguageExt;
+using OpenttdDiscord.Base.Basics;
+using OpenttdDiscord.Base.Ext;
+using OpenttdDiscord.Domain.Security;
+using OpenttdDiscord.Infrastructure.Discord.Responses;
+using OpenttdDiscord.Infrastructure.Discord.Runners;
+
+namespace OpenttdDiscord.Infrastructure.Reporting.Runners
+{
+    internal class RegisterReportChannelRunner : OttdSlashCommandRunnerBase
+    {
+        protected override EitherAsync<IError, ISlashCommandResponse> RunInternal(SocketSlashCommand command, User user, ExtDictionary<string, object> options)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

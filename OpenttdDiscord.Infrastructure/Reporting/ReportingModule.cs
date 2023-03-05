cca0f73dfeac77a6deeba56bc490cefe85e30f42
Damian Laczak <shoter@users.noreply.github.com>
2023-03-06 00:23:03 +0100

    feat: ReportingModule

---
+++ OpenttdDiscord.Infrastructure/Reporting/ReportingModule.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using OpenttdDiscord.Database.Reporting;
+using OpenttdDiscord.Infrastructure.Modularity;
+
+namespace OpenttdDiscord.Infrastructure.Reporting
+{
+    internal class ReportingModule : IModule
+    {
+        public void RegisterDependencies(IServiceCollection services)
+        {
+            services.AddScoped<IReportChannelRepository, ReportChannelRepository>();
+            services
+                .RegisterUseCases()
+                .RegisterCommands()
+                .RegisterRunners();
+        }
+    }
+
+#pragma warning disable SA1402 // File may only contain a single type
+    internal static class ReportingModuleExtensions
+#pragma warning restore SA1402 // File may only contain a single type
+    {
+        public static IServiceCollection RegisterUseCases(this IServiceCollection services)
+        {
+            return services;
+        }
+
+        public static IServiceCollection RegisterRunners(this IServiceCollection services)
+        {
+            return services;
+        }
+
+        public static IServiceCollection RegisterCommands(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}

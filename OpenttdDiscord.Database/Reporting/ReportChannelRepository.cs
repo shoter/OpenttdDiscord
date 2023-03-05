ee28e06240147530949c1222122a6eb70ce73697
Damian Laczak <shoter@users.noreply.github.com>
2023-03-06 00:35:00 +0100

    feat: ReportChannelRepositoryShould

--- OpenttdDiscord.Database/Reporting/ReportChannelRepository.cs
+++ OpenttdDiscord.Database/Reporting/ReportChannelRepository.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using LanguageExt;
+﻿using LanguageExt;
 using Microsoft.EntityFrameworkCore;
 using OpenttdDiscord.Domain.Reporting;
 

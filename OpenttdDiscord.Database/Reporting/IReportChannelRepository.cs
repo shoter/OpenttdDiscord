272a6ee628f72d2c7094424a17f0fffcf356befa
Damian Laczak <shoter@users.noreply.github.com>
2023-03-06 00:21:20 +0100

    feat: ReportChannelRepository

---
+++ OpenttdDiscord.Database/Reporting/IReportChannelRepository.cs
@@ -0,0 +1,16 @@
+﻿using LanguageExt;
+using OpenttdDiscord.Domain.Reporting;
+
+namespace OpenttdDiscord.Database.Reporting
+{
+    internal interface IReportChannelRepository
+    {
+        EitherAsyncUnit Insert(ReportChannel chatChannel);
+
+        EitherAsyncUnit Delete(Guid serverId, ulong channelId);
+
+        EitherAsync<IError, List<ReportChannel>> GetReportChannel(Guid serverId);
+
+        EitherAsync<IError, Option<ReportChannel>> GetReportChannelsForServer(Guid serverId, ulong channelId);
+    }
+}
---
+++ OpenttdDiscord.Database/Reporting/ReportChannelRepository.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LanguageExt;
+using Microsoft.EntityFrameworkCore;
+using OpenttdDiscord.Domain.Reporting;
+
+namespace OpenttdDiscord.Database.Reporting
+{
+    internal class ReportChannelRepository : IReportChannelRepository
+    {
+        private OttdContext DB { get; }
+
+        public ReportChannelRepository(OttdContext dB)
+        {
+            DB = dB;
+        }
+
+        public EitherAsyncUnit Delete(Guid serverId, ulong channelId)
+            => TryAsync<EitherUnit>(async () =>
+            {
+                int deletedRows = await DB.ReportChannels
+                    .Where(rc => rc.ServerId == serverId && rc.ChannelId == channelId)
+                    .DeleteFromQueryAsync();
+
+                if (deletedRows == 0)
+                {
+                    return new HumanReadableError("No report channel was found for deletion");
+                }
+
+                return Unit.Default;
+            }).ToEitherAsyncErrorFlat();
+
+        public EitherAsync<IError, List<ReportChannel>> GetReportChannel(Guid serverId)
+            => TryAsync<Either<IError, List<ReportChannel>>>(async () =>
+            {
+                return (await DB.ReportChannels
+                    .AsNoTracking()
+                    .Where(rc => rc.ServerId == serverId)
+                    .ToListAsync())
+                    .Select(rc => rc.ToDomain())
+                    .ToList();
+            }).ToEitherAsyncErrorFlat();
+
+        public EitherAsyncUnit Insert(ReportChannel reportChannel)
+            => TryAsync<EitherUnit>(async () =>
+            {
+                await DB.ReportChannels.AddAsync(new(reportChannel));
+                await DB.SaveChangesAsync();
+                return Unit.Default;
+            }).ToEitherAsyncErrorFlat();
+
+        public EitherAsync<IError, Option<ReportChannel>> GetReportChannelsForServer(Guid serverId, ulong channelId)
+              => TryAsync<Either<IError, Option<ReportChannel>>>(async () =>
+              {
+                  var reportChannel = await DB.ReportChannels
+                      .AsNoTracking()
+                      .FirstOrDefaultAsync(rc => rc.ServerId == serverId && rc.ChannelId == channelId);
+
+                  if (reportChannel == null)
+                  {
+                      return Option<ReportChannel>.None;
+                  }
+
+                  return Option<ReportChannel>.Some(reportChannel.ToDomain());
+              }).ToEitherAsyncErrorFlat();
+    }
+}

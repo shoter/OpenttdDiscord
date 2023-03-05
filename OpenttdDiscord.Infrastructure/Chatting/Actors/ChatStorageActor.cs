30a6c320e10bcf72252ce8fef9c57e7b14e7ccab
Damian Laczak <shoter@users.noreply.github.com>
2023-03-05 12:32:34 +0100

    feat: diagrams for chat storage

---
+++ OpenttdDiscord.Infrastructure/Chatting/Actors/ChatStorageActor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenttdDiscord.Infrastructure.Chatting.Actors
+{
+    /// <summary>
+    /// Used to store chat messages for given ottd server. It automatically connects to the ottd server event chain to gather chat messages.
+    /// </summary>
+    internal class ChatStorageActor
+    {
+        private readonly 
+    }
+}
---
+++ docs/readme.md
@@ -0,0 +1,4 @@
+To edit diagrams please use [draw.io plugin](https://marketplace.visualstudio.com/items?itemName=hediet.vscode-drawio).
+Do not edit diagrams by any painting program or you will ruin diagram data inside them.
+
+All PRs that would contain modifications done by something else than draw.io plugin will be rejected.

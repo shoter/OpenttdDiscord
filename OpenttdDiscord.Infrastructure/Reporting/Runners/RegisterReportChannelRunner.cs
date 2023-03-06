389c705b172ee54b4c2175ecaf5eb0640ead0bf3
Damian Laczak <shoter@users.noreply.github.com>
2023-03-06 01:13:31 +0100

    feat: RegisterReportChannelRunner

--- OpenttdDiscord.Infrastructure/Reporting/Runners/RegisterReportChannelRunner.cs
+++ OpenttdDiscord.Infrastructure/Reporting/Runners/RegisterReportChannelRunner.cs
@@ -1,8 +1,12 @@
 ﻿using Discord.WebSocket;
 using LanguageExt;
+using Microsoft.EntityFrameworkCore.Metadata;
 using OpenttdDiscord.Base.Basics;
 using OpenttdDiscord.Base.Ext;
+using OpenttdDiscord.Domain.Reporting;
+using OpenttdDiscord.Domain.Reporting.UseCases;
 using OpenttdDiscord.Domain.Security;
+using OpenttdDiscord.Domain.Servers.UseCases;
 using OpenttdDiscord.Infrastructure.Discord.Responses;
 using OpenttdDiscord.Infrastructure.Discord.Runners;
 
@@ -10,9 +14,26 @@ namespace OpenttdDiscord.Infrastructure.Reporting.Runners
 {
     internal class RegisterReportChannelRunner : OttdSlashCommandRunnerBase
     {
+        private readonly IGetServerUseCase getServerUseCase;
+
+        private readonly IRegisterReportChannelUseCase registerReportChannelUseCase;
+
+        public RegisterReportChannelRunner(IGetServerUseCase getServerUseCase, IRegisterReportChannelUseCase registerReportChannelUseCase)
+        {
+            this.getServerUseCase = getServerUseCase;
+            this.registerReportChannelUseCase = registerReportChannelUseCase;
+        }
+
         protected override EitherAsync<IError, ISlashCommandResponse> RunInternal(SocketSlashCommand command, User user, ExtDictionary<string, object> options)
         {
-            throw new NotImplementedException();
+            string serverName = options.GetValueAs<string>("server-name");
+            ulong guildId = command.GuildId!.Value;
+            ulong channelId = command.ChannelId!.Value;
+
+            return
+                from server in getServerUseCase.Execute(user, serverName, guildId)
+                from _1 in registerReportChannelUseCase.Execute(user, new ReportChannel(server.Id, guildId, channelId))
+                select (ISlashCommandResponse)new TextCommandResponse("Report channel registered");
         }
     }
 }

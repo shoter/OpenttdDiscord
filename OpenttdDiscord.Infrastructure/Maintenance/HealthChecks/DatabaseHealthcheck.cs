cd0c50a6e90a7a324c141c2ccd4a1e65a32946c2
Damian Laczak <shoter@users.noreply.github.com>
2023-06-01 00:35:19 +0200

    feat: DatabaseHealthcheck

---
+++ OpenttdDiscord.Infrastructure/Maintenance/HealthChecks/DatabaseHealthcheck.cs
@@ -0,0 +1,52 @@
+using System.Diagnostics;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Logging;
+using OpenttdDiscord.Domain.Servers;
+
+namespace OpenttdDiscord.Infrastructure.Maintenance.HealthChecks
+{
+    public class DatabaseHealthcheck : IHealthCheck
+    {
+        private readonly IOttdServerRepository serverRepository;
+        private readonly ILogger logger;
+
+        public DatabaseHealthcheck(
+            IOttdServerRepository serverRepository,
+            ILogger<DatabaseHealthcheck> logger)
+        {
+            this.serverRepository = serverRepository;
+            this.logger = logger;
+        }
+
+        public async Task<HealthCheckResult> CheckHealthAsync(
+            HealthCheckContext context,
+            CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                Stopwatch stopWatch = new();
+                stopWatch.Start();
+                await serverRepository.GetAllGuilds();
+                stopWatch.Stop();
+
+                var time = stopWatch.Elapsed;
+
+                if (time.TotalSeconds > 1.0)
+                {
+                    return HealthCheckResult.Degraded("Response time for SQL query was longer than 1 second");
+                }
+
+                return HealthCheckResult.Healthy();
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(
+                    ex,
+                    "Something when wrong during database health check");
+                return HealthCheckResult.Unhealthy(
+                    "Something when wrong with database health check",
+                    ex);
+            }
+        }
+    }
+}

c369f247856ed9b21d346df6f532b1189cb67e20
Damian Laczak <shoter@users.noreply.github.com>
2023-08-06 17:11:57 +0200

    test: AkkaServiceShould

---
+++ OpenttdDiscord.Infrastructure.Tests/Akkas/AkkaServiceShould.cs
@@ -0,0 +1,54 @@
+using Akka.Actor;
+using OpenttdDiscord.Base.Ext;
+using OpenttdDiscord.Infrastructure.Akkas;
+using Xunit.Abstractions;
+
+namespace OpenttdDiscord.Infrastructure.Tests.Akkas
+{
+    public class AkkaServiceShould : BaseActorTestKit
+    {
+        private readonly AkkaService akkaService;
+
+        public AkkaServiceShould(ITestOutputHelper helper)
+            : base(helper)
+        {
+            akkaService = new(Sys);
+            akkaService.NotifyAboutAkkaStart();
+        }
+
+        [Fact]
+        public async Task SelectActor()
+        {
+            // Arrange
+            var testProbe = CreateTestProbe("2137");
+
+            // Act
+            var selection = (await akkaService.SelectActor("/system/2137")).Right();
+
+            // Assert
+            selection.Tell("2137");
+            testProbe.ExpectMsg<string>("2137");
+        }
+
+        [Fact]
+        public async Task SelectActor_AndSendItAMessage()
+        {
+            // Arrange
+            var testProbe = CreateTestProbe("2137");
+            string response = "JeszczeJak";
+
+            // Act
+            var askTask = akkaService.SelectAndAsk<string>(
+                "/system/2137",
+                "2137");
+
+            // Assert
+            testProbe.ExpectMsg<string>("2137");
+            testProbe.Sender.Tell(response);
+
+            Assert.Equal(
+                response,
+                (await askTask).Right());
+        }
+    }
+}

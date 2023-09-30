c675756b1843050b6c4f9634bdf16dab5646ee9a
Damian Laczak <shoter@users.noreply.github.com>
2023-09-30 19:26:32 +0200

    test: HealthCheckRunnerShould

---
+++ OpenttdDiscord.Infrastructure.Tests/Maintenance/Runners/HealthCheckRunnerShould.cs
@@ -0,0 +1,32 @@
+using OpenttdDiscord.Domain.Roles.Errors;
+using OpenttdDiscord.Domain.Security;
+using OpenttdDiscord.Infrastructure.Maintenance.Runners;
+
+namespace OpenttdDiscord.Infrastructure.Tests.Maintenance.Runners
+{
+    public class HealthCheckRunnerShould : RunnerTestBase
+    {
+        private readonly HealthCheckRunner sut;
+
+        public HealthCheckRunnerShould()
+        {
+            sut = new(
+                akkaServiceSub,
+                getRoleLevelUseCaseSub);
+        }
+
+        [Theory]
+        [InlineData(UserLevel.User)]
+        [InlineData(UserLevel.Moderator)]
+        public async Task NotExecuteForNonAdmin(UserLevel userLevel)
+        {
+            var result = await WithGuildUser()
+                .WithOption("server-name", "whatever")
+                .WithUserLevel(userLevel)
+                .RunExt(sut);
+
+            Assert.True(result.IsLeft);
+            Assert.True(result.Left() is IncorrectUserLevelError);
+        }
+    }
+}

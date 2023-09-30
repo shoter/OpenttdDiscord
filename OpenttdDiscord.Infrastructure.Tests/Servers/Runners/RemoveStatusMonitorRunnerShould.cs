bbc83a05d301f18906e4c2d59631f17002836bb8
Damian Laczak <shoter@users.noreply.github.com>
2023-09-30 20:11:19 +0200

    test: RemoveStatusMonitorRunnerShould

---
+++ OpenttdDiscord.Infrastructure.Tests/Servers/Runners/RemoveStatusMonitorRunnerShould.cs
@@ -0,0 +1,41 @@
+using OpenttdDiscord.Domain.Roles.Errors;
+using OpenttdDiscord.Domain.Security;
+using OpenttdDiscord.Domain.Servers;
+using OpenttdDiscord.Domain.Statuses.UseCases;
+using OpenttdDiscord.Infrastructure.Statuses.Runners;
+
+namespace OpenttdDiscord.Infrastructure.Tests.Servers.Runners
+{
+    public class RemoveStatusMonitorRunnerShould : RunnerTestBase
+    {
+        private readonly IRemoveStatusMonitorUseCase removeStatusMonitorUseCaseSub =
+            Substitute.For<IRemoveStatusMonitorUseCase>();
+
+        private readonly IOttdServerRepository ottdServerRepositorySub = Substitute.For<IOttdServerRepository>();
+
+        private readonly RemoveStatusMonitorRunner sut;
+
+        public RemoveStatusMonitorRunnerShould()
+        {
+            sut = new(
+                removeStatusMonitorUseCaseSub,
+                ottdServerRepositorySub,
+                akkaServiceSub,
+                getRoleLevelUseCaseSub);
+        }
+
+        [Theory]
+        [InlineData(UserLevel.User)]
+        [InlineData(UserLevel.Moderator)]
+        public async Task NotExecuteForNonAdmin(UserLevel userLevel)
+        {
+            var result = await WithGuildUser()
+                .WithOption("server-name", "whatever")
+                .WithUserLevel(userLevel)
+                .RunExt(sut);
+
+            Assert.True(result.IsLeft);
+            Assert.True(result.Left() is IncorrectUserLevelError);
+        }
+    }
+}

6118389e8fbc4bc093b69a3c0f967627ece80269
Damian Laczak <shoter@users.noreply.github.com>
2023-09-30 19:36:34 +0200

    test: UnregisterRconChannelRunnerShould

--- OpenttdDiscord.Infrastructure.Tests/Rcon/Runners/UnregisterRconChannelRunnerShould.cs
+++ OpenttdDiscord.Infrastructure.Tests/Rcon/Runners/UnregisterRconChannelRunnerShould.cs
@@ -1,4 +1,3 @@
-using OpenttdDiscord.Domain.Chatting.UseCases;
 using OpenttdDiscord.Domain.Rcon.UseCases;
 using OpenttdDiscord.Domain.Roles.Errors;
 using OpenttdDiscord.Domain.Security;
@@ -7,32 +6,31 @@ using OpenttdDiscord.Infrastructure.Rcon.Runners;
 
 namespace OpenttdDiscord.Infrastructure.Tests.Rcon.Runners
 {
-    public class ListRconChannelsRunnerShould : RunnerTestBase
+    public class UnregisterRconChannelRunnerShould : RunnerTestBase
     {
         private readonly IGetServerUseCase getServerUseCaseSubsitute = Substitute.For<IGetServerUseCase>();
 
-        private readonly IListRconChannelsUseCase listRconChannelsUseCaseSub =
-            Substitute.For<IListRconChannelsUseCase>();
+        private readonly IUnregisterRconChannelUseCase unregisterRconChannelUseCaseSubstitute =
+            Substitute.For<IUnregisterRconChannelUseCase>();
 
-        private readonly ListRconChannelsRunner sut;
+        private readonly UnregisterRconChannelRunner sut;
 
-        public ListRconChannelsRunnerShould()
+        public UnregisterRconChannelRunnerShould()
         {
             sut = new(
                 getServerUseCaseSubsitute,
-                listRconChannelsUseCaseSub,
+                unregisterRconChannelUseCaseSubstitute,
                 akkaServiceSub,
                 getRoleLevelUseCaseSub);
         }
 
         [Theory]
         [InlineData(UserLevel.User)]
-        public async Task NotExecuteForNonModerator(UserLevel userLevel)
+        [InlineData(UserLevel.Moderator)]
+        public async Task NotExecuteForNonAdmin(UserLevel userLevel)
         {
             var result = await WithGuildUser()
-                .WithOption(
-                    "server-name",
-                    "whatever")
+                .WithOption("server-name", "whatever")
                 .WithUserLevel(userLevel)
                 .RunExt(sut);
 

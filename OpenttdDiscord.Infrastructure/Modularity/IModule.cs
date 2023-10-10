b765d94217395ff0ed7c0239b6c6b0ac3c3d8dd8
Damian Laczak <shoter@users.noreply.github.com>
2023-10-11 01:41:54 +0200

    test: IModule

--- OpenttdDiscord.Infrastructure/Modularity/IModule.cs
+++ OpenttdDiscord.Infrastructure/Modularity/IModule.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace OpenttdDiscord.Infrastructure.Modularity
 {
+    [ExcludeFromCodeCoverage]
     internal interface IModule
     {
         public void RegisterDependencies(IServiceCollection services);

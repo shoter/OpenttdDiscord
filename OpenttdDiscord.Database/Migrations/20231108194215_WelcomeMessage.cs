e3b4178d93e36041e798ce9ed80ce0593a869dac
Damian Laczak <shoter@users.noreply.github.com>
2023-11-11 18:02:58 +0100

    test: exclude migrations from test cvr

--- OpenttdDiscord.Database/Migrations/20231108194215_WelcomeMessage.cs
+++ OpenttdDiscord.Database/Migrations/20231108194215_WelcomeMessage.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
@@ -6,6 +7,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace OpenttdDiscord.Database.Migrations
 {
     /// <inheritdoc />
+    [ExcludeFromCodeCoverage]
     public partial class WelcomeMessage : Migration
     {
         /// <inheritdoc />
